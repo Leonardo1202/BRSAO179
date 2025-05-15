4d8e554765dbea88ed8b8228fdbf66670fd9663a
Leonardo <leolima.custodio@hotmail.com>
2025-05-15 20:23:51 -0300

    preco_com_desconto.py

---
+++ exercicios/preco_com_desconto.py
@@ -0,0 +1,6 @@
+
+preco= 1000 #valor do produto 
+desconto = 15 #valor do produto
+
+valor_final = preco - (preco * desconto / 100) #formula do desconto
+print(f"\no preço de celular de Maria com {desconto}% de desconto é R${valor_final}")
